d02ad0cd88fe5ab9f5bdab82edb7bc76139811c5
Takuya-Ampi <ap.takuya@gmail.com>
2022-11-09 01:09:15 +0900

    first commit

---
+++ app.vue
@@ -0,0 +1,16 @@
+<script setup>
+  const showMessageFromBackend = async () => {
+    try {
+      const { data } = await useFetch('/server-api/test')
+      console.log(data.value)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+</script>
+
+<template>
+  <div>
+    <button @click="showMessageFromBackend">Show message from backend</button>
+  </div>
+</template>
